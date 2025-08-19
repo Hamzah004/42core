d5d3966dc7dbae31c5af67de18ac4072d3016239
Hamzeh Bani Ata <hbani-at@c2r3s5.42amman.com>
2025-08-19 14:21:36 +0300

    added ft_putnbr_fd fix

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   by: hbani-at <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hbani-at <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   created: 2025/08/17 21:53:34 by hbani-at          #+#    #+#             */
-/*   updated: 2025/08/17 21:54:10 by hbani-at         ###   ########.fr       */
+/*   Created: 2025/08/19 14:01:12 by hbani-at          #+#    #+#             */
+/*   Updated: 2025/08/19 14:10:14 by hbani-at         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putnbr_fd(int n, int fd) // 1234
+void	ft_putnbr_fd(int n, int fd)
 {
 	long	number;
 
@@ -23,11 +23,11 @@ void	ft_putnbr_fd(int n, int fd) // 1234
 		ft_putchar_fd('-', fd);
 		number *= -1;
 	}
-        if (n >= 0 && n <= 9)
-                ft_putchar_fd(n + '0',fd);
+	if (number >= 0 && number <= 9)
+		ft_putchar_fd(number + '0', fd);
 	if (number > 9)
 	{
-		ft_putnbr_fd(number / 10,fd);
-		ft_putnbr_fd(number % 10,fd);
+		ft_putnbr_fd(number / 10, fd);
+		ft_putnbr_fd(number % 10, fd);
 	}
 }
