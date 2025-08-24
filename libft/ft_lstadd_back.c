42ba51eaa85bb116e9b2d9e496b34ac2c7afc4e9
Hamzeh Bani Ata <hbani-at@c2r3s2.42amman.com>
2025-08-24 21:14:14 +0300

    added add back

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,22 +6,25 @@
 /*   By: hbani-at <hbani-at@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 20:18:51 by hbani-at          #+#    #+#             */
-/*   Updated: 2025/08/24 20:22:35 by hbani-at         ###   ########.fr       */
+/*   Updated: 2025/08/24 21:13:54 by hbani-at         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *temp;
+	t_list	*last;
 
-	*lst = temp;
-	if (!lst)
-		return (NULL);
-	while (temp->next)
+	if (!*lst)
 	{
-		
+		*lst = new;
+		return ;
 	}
-
+	last = *lst;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = new;
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hbani-at <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 20:41:53 by hbani-at          #+#    #+#             */
-/*   Updated: 2025/08/24 20:16:19 by hbani-at         ###   ########.fr       */
+/*   Updated: 2025/08/24 20:33:14 by hbani-at         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,4 +67,5 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
 #endif
