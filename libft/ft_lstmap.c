b7c213655eed1294c0af0cf3a83af68daaaa85ae
Hamzah004 <hamzahbaniata75@gmail.com>
2025-08-25 15:25:43 +0300

    added bouns full

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbani-at <hbani-at@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 14:24:02 by hbani-at          #+#    #+#             */
+/*   Updated: 2025/08/25 15:23:01 by hbani-at         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_node;
+	t_list	*new_list;
+	void	*node_content;
+	if (!lst || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		node_content = f(lst->content);
+		new_node = ft_lstnew(node_content);
+		if (!new_node)
+		{
+			if (del && node_content)
+			{
+			del(new_node->content);
+			ft_lstclear(&new_list,del);
+				return (NULL);
+			}
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hbani-at <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 20:41:53 by hbani-at          #+#    #+#             */
-/*   Updated: 2025/08/24 21:18:42 by hbani-at         ###   ########.fr       */
+/*   Updated: 2025/08/25 15:08:32 by hbani-at         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,8 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
-void ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
