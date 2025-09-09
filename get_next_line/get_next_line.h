881b653f83a033306a23caa3e8337823c8fa064d
Hamzah004 <hamzahbaniata75@gmail.com>
2025-09-09 20:58:48 +0300

    added project sturcture and added material

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbani-at <hbani-at@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 20:52:41 by hbani-at          #+#    #+#             */
+/*   Updated: 2025/09/09 20:52:42 by hbani-at         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbani-at <hbani-at@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 20:49:58 by hbani-at          #+#    #+#             */
+/*   Updated: 2025/09/09 20:51:08 by hbani-at         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//:NOTE: this is a main file used for testing,
+make sure to delete it after your done with the project
+
+	int
+	main(void)
+{
+}
