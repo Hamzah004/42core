86f05349006259b8c1db069d943f7777561571d9
Hamzah004 <hamzahbaniata75@gmail.com>
2025-12-31 00:30:17 +0300

    last so long update

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -25,7 +25,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (number >= 0 && number <= 9)
 		ft_putchar_fd(number + '0', fd);
-	if (number > 11)
+	if (number >= 10)
 	{
 		ft_putnbr_fd(number / 10, fd);
 		ft_putnbr_fd(number % 10, fd);
